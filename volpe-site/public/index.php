da9c29210c5d739137b4617f07e1172e313a50ef
lilyy13 <marilou.geay@eleve.isep.fr>
2025-06-13 14:25:19 +0200

    ajout paragraphe présentation volope

--- volpe-site/public/index.php
+++ volpe-site/public/index.php
@@ -18,7 +18,10 @@
 
   <section class="intro">
     <h2>Le projet</h2>
-    <p>Volpe vous propose une solution complète de suivi d’activité dans une salle de classe : détection de présence, alertes environnementales (température, CO2), tableaux d’analyse, et export PDF sécurisé.</p>
+    <p>Nous sommes volpe. Notre but est d'améliorer les conditions dans la salle de classe. Grâce à nos nombreux capteurs nous récoltons des informations dans une salle de classe, vous pouvez connaître la température de la salle, le nombre d’élèves présent dans la salle, ou encore le taux de gaz dans la pièce. 
+Avec ses informations rassemblées sur des graphiques nous vous proposons des solutions pour améliorer les conditions de travail des élèves. 
+Nous vous proposons des buzzer qui signal lorsque la température dépasse un seuil que nous définissons ensembles au préalable. Cela pourra permettre de réduire les factures de chauffage en évitant de surchauffer. 
+Nous vous proposons aussi un buzzer pour le taux de gaz. Il peut être très utile dans les salles de chimie ou de SVT. En salle de chimie le buzzer peut indiquer quand il est nécessaire d'arrêter la pièce pour faire baisser le taux de gaz. Pour les salle de SVT le buzzer peut servir d'indication de mesures sur des plantes qui relâche beaucoup de Co2.</p>
   </section>
 
   <section class="features">
